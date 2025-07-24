2b22b856960d3c9231fa0ca6c4fd98159708a3e4
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-07-24 17:15:46 -0500

    Call new component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Main from './components/Main.vue'
+import InformationComponent from './components/InformationComponent.vue'
 </script>
 
 <template>
   <Main />
+  <InformationComponent />
 </template>
