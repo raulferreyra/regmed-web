ecd43d9d568bcc8ec947331f302132d3845bcd3c
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-07-24 17:59:36 -0500

    Added New Components

--- src/components/InformationComponent.vue
+++ src/components/InformationComponent.vue
@@ -3,23 +3,26 @@
         <article>
             <h2 v-if="block.title">{{ block.title }}</h2>
             <p v-if="block.content" v-html="formatHtml(block.content)"></p>
-
             <img v-if="block.image" :src="block.image" alt="" />
+            <!-- Aquí usas tu componente TableComponent, pasándole el nombre de la tabla -->
+            <TableComponent v-if="block.table && block.name_table" :name="block.name_table" />
+            <!-- Y aquí el componente para el árbol -->
+            <TreeComponent v-if="block.tree" />
         </article>
     </section>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import TableComponent from './TableComponent.vue'
+import TreeComponent from './TreeComponent.vue'
 
 const content = ref([])
-const formatHtml = (text) => {
-    return text?.replace(/\n/g, '<br/>') || ''
-}
+const formatHtml = (text) => text?.replace(/\n/g, '<br/>') || ''
 
 onMounted(async () => {
     try {
-        const res = await fetch('./json/regmed_page_content.json')
+        const res = await fetch('/json/regmed_page_content.json')
         if (!res.ok) throw new Error('No se pudo cargar el JSON')
         content.value = await res.json()
     } catch (err) {
