f1636f3938cab177cf50563d13ec67da15d067df
Raúl Ferreyra <raulrferreyrav@gmail.com>
2025-07-24 17:06:33 -0500

    Put content for principal page

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,3 +1,28 @@
-<template class="main">
-  Hola
+<template>
+  <section class="main" v-for="(section, index) in sections" :key="index">
+    <article>
+      <div class="content">
+        <h1 v-if="section.title">{{ section.title }}</h1>
+        <h2 v-if="section.subtitle">{{ section.subtitle }}</h2>
+        <p v-if="section.content" v-html="section.content"></p>
+      </div>
+      <img v-if="section.image" :src="section.image" alt="" />
+    </article>
+  </section>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const sections = ref([])
+
+onMounted(async () => {
+  try {
+    const res = await fetch('./json/regmed_principal_content.json')
+    if (!res.ok) throw new Error('No se pudo cargar el JSON.')
+    sections.value = await res.json()
+  } catch (err) {
+    console.error('Error cargando JSON:', err)
+  }
+})
+</script>
